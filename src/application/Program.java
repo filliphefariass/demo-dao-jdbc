09d2c4e677087fd00793866cd1e343800a251807
filliphe <filliphesouza506@gmail.com>
2025-03-03 12:40:38 -0300

    Seller enity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Saller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		
+		Saller saller = new Saller(21, "Bob", null, new Date(), 3000.0, obj);
+		
+		System.out.println(saller);
 	}
 
 }
